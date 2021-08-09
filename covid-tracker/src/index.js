05b894ba67cc2e417c94be94ca4d022812789369
ngwatso <ngwatson79@yahoo.com>
2021-08-09 19:47:28 -0400

    set up state and middleware for app

--- covid-tracker/src/index.js
+++ covid-tracker/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import { covidReducer } from './utils/reducers/covidReducer';
+
+const store = createStore(covidReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
