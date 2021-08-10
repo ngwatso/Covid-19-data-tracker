1f6f7bfd362eba3e2d6d12bc8cbe0019b1fff8ba
ngwatso <ngwatson79@yahoo.com>
2021-08-09 21:52:02 -0400

    added dependancies, did some minor styling

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,14 +1,60 @@
 /**
  * @prettier
  */
-
-import logo from './logo.svg';
 import './App.css';
+import { generate, presetDarkPallettes } from '@ant-design/colors';
+import { Layout, Avatar } from 'antd';
+import Title from 'antd/lib/typography/Title';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
+	const colors = generate('#1890ff', {
+		theme: 'dark',
+		backgroundColor: '141414',
+	});
+	console.log(colors);
+
 	return (
 		<div className="App">
-			<h1> Covid Tracker</h1>
+			<Layout>
+				<Header
+					style={{
+						padding: 5,
+						background: '#b7e3fa',
+					}}
+				>
+					<Avatar
+						size={50}
+						icon="user"
+						style={{
+							float: 'left',
+						}}
+					/>
+					<Title level={1}>
+						Covid-19 Data
+						Tracker
+					</Title>
+				</Header>
+				<Layout>
+					<Sider
+						style={{
+							background: '#65b7f3',
+						}}
+					>
+						Sider
+					</Sider>
+					<Layout>
+						<Content>
+							Content
+						</Content>
+					</Layout>
+				</Layout>
+			</Layout>
+
+			<Layout>
+				<Footer>Footer</Footer>
+			</Layout>
 		</div>
 	);
 }
